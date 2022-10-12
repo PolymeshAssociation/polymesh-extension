855aba0d6d9e0339672c89bfa1ee2f95cda900ae
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-10-13 00:56:10 +0800

    Add support for call_runtime/read_storage interface.

--- examples/extension-tester/lib.rs
+++ examples/extension-tester/lib.rs
@@ -5,9 +5,26 @@ extern crate alloc;
 use ink_lang as ink;
 use alloc::vec::Vec;
 
-use polymesh_extension::*;
+use polymesh_api::{
+  Api,
+  ink::{
+    extension::PolymeshEnvironment,
+    basic_types::IdentityId,
+    Error as PolymeshError,
+  },
+  polymesh::types::{
+    polymesh_primitives::{
+      ticker::Ticker,
+      asset::{
+        AssetName,
+        AssetType,
+      },
+      identity_id::PortfolioId,
+    },
+  },
+};
 
-#[ink::contract(env = polymesh_extension::PolymeshEnvironment)]
+#[ink::contract(env = PolymeshEnvironment)]
 mod runtime_tester {
     use alloc::vec;
 
@@ -18,14 +35,17 @@ mod runtime_tester {
     pub struct RuntimeTester {}
 
     /// The contract error types.
-    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[derive(Debug, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         /// Caller needs to pay the contract for the protocol fee.
         /// (Amount needed)
         InsufficientTransferValue(Balance),
         /// Polymesh runtime error.
-        PolymeshError(PolymeshRuntimeErr),
+        PolymeshError(PolymeshError),
+        //PolymeshError(PolymeshRuntimeErr),
+        /// Scale decode failed.
+        ScaleError,
     }
 
     // hard-code protocol fees.
@@ -41,42 +61,92 @@ mod runtime_tester {
         pub fn new() -> Self { Self {} }
 
         #[ink(message)]
+        pub fn call_runtime(&mut self, call: Vec<u8>) -> Result<()> {
+            Self::env().extension().call_runtime(call.into())
+              .map_err(|err| Error::PolymeshError(PolymeshError::RuntimeError(err)))
+        }
+
+        #[ink(message)]
+        pub fn read_storage(&self, key: Vec<u8>) -> Result<Option<Vec<u8>>> {
+            Self::env().extension().read_storage(key.into())
+              .map_err(|err| Error::PolymeshError(PolymeshError::RuntimeError(err)))
+        }
+
+        #[ink(message)]
+        pub fn get_spec_version(&self) -> Result<u32> {
+            Self::env().extension().get_spec_version()
+              .map_err(|err| Error::PolymeshError(PolymeshError::RuntimeError(err)))
+        }
+
+        #[ink(message)]
+        pub fn get_transaction_version(&self) -> Result<u32> {
+            Self::env().extension().get_transaction_version()
+              .map_err(|err| Error::PolymeshError(PolymeshError::RuntimeError(err)))
+        }
+
+        #[ink(message)]
+        pub fn asset_balance_of(&self, ticker: Ticker, did: IdentityId) -> Result<u128> {
+            let api = Api::new();
+            api.query().asset().balance_of(ticker, did)
+              .map_err(|err| Error::PolymeshError(err))
+        }
+
+        #[ink(message)]
+        pub fn portfolio_asset_balance(&self, portfolio: PortfolioId, ticker: Ticker) -> Result<u128> {
+            let api = Api::new();
+            api.query().portfolio().portfolio_asset_balances(portfolio, ticker)
+              .map_err(|err| Error::PolymeshError(err))
+        }
+
+        #[ink(message)]
         pub fn register_ticker(&mut self, ticker: Ticker) -> Result<()> {
-            Self::env().extension().register_ticker(ticker)
+            let api = Api::new();
+            api.call().asset().register_ticker(ticker).submit()
               .map_err(|err| Error::PolymeshError(err))
         }
 
         #[ink(message)]
         pub fn accept_ticker_transfer(&mut self, auth_id: u64) -> Result<()> {
-            Self::env().extension().accept_ticker_transfer(auth_id)
+            let api = Api::new();
+            api.call().asset().accept_ticker_transfer(auth_id).submit()
               .map_err(|err| Error::PolymeshError(err))
         }
 
         #[ink(message)]
         pub fn accept_asset_ownership_transfer(&mut self, auth_id: u64) -> Result<()> {
-            Self::env().extension().accept_asset_ownership_transfer(auth_id)
+            let api = Api::new();
+            api.call().asset().accept_asset_ownership_transfer(auth_id).submit()
+              .map_err(|err| Error::PolymeshError(err))
+        }
+
+        fn create_asset(&mut self, name: AssetName, ticker: Ticker, asset_type: AssetType, supply: u128) -> Result<()> {
+            let api = Api::new();
+            api.call().asset().create_asset(name, ticker.clone(), true, asset_type, vec![], None, true).submit()
+              .map_err(|err| Error::PolymeshError(err))?;
+            api.call().asset().issue(ticker, supply).submit()
               .map_err(|err| Error::PolymeshError(err))
         }
 
         #[ink(message)]
-        pub fn create_asset(&mut self, name: AssetName, ticker: Ticker, asset_type: AssetType) -> Result<()> {
-            Self::env().extension().create_asset(name, ticker, true, asset_type, vec![], None, true)
-              .map_err(|err| Error::PolymeshError(err))
+        /// Create asset where the contract pays the asset creation fees (3k POLYX).
+        pub fn create_asset_and_issue(&mut self, name: AssetName, ticker: Ticker, asset_type: AssetType, supply: u128) -> Result<()> {
+            self.create_asset(name, ticker, asset_type, supply)
         }
 
         #[ink(message, payable)]
-        pub fn payable_create_asset(&mut self, name: AssetName, ticker: Ticker, asset_type: AssetType) -> Result<()> {
+        /// Create asset where the caller need to pay the asset creation fees (3k POLYX).
+        pub fn payable_create_asset_and_issue(&mut self, name: AssetName, ticker: Ticker, asset_type: AssetType, supply: u128) -> Result<()> {
             let transferred = Self::env().transferred_value();
             if transferred < CREATE_ASSET_FEE {
               return Err(Error::InsufficientTransferValue(CREATE_ASSET_FEE));
             }
-            Self::env().extension().create_asset(name, ticker, true, asset_type, vec![], None, true)
-              .map_err(|err| Error::PolymeshError(err))
+            self.create_asset(name, ticker, asset_type, supply)
         }
 
         #[ink(message)]
         pub fn register_custom_asset_type(&mut self, ty: Vec<u8>) -> Result<()> {
-            Self::env().extension().register_custom_asset_type(ty)
+            let api = Api::new();
+            api.call().asset().register_custom_asset_type(ty).submit()
               .map_err(|err| Error::PolymeshError(err))
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -2,142 +2,67 @@
 
 extern crate alloc;
 
-use ink_lang as ink;
 use ink_env::Environment;
+use ink_lang as ink;
 
-#[cfg(feature = "type_info")]
+use scale::{Encode, Output};
+
+#[cfg(feature = "std")]
 use scale_info::TypeInfo;
 
-use scale::{Decode, Encode};
 use alloc::vec::Vec;
 
-pub const TICKER_LEN: usize = 12;
+/// `Encoded` is used to avoid encoding an extra length that isn't needed.
+pub struct Encoded(pub Vec<u8>);
 
-pub type Ticker = [u8; TICKER_LEN];
-
-/// A wrapper for a token name.
-#[derive(Clone, Decode, Encode)]
-#[cfg_attr(feature = "type_info", derive(TypeInfo))]
-pub struct AssetName(pub Vec<u8>);
-
-/// The ID of a custom asset type.
-#[derive(Clone, Copy, Decode, Encode)]
-#[cfg_attr(feature = "type_info", derive(TypeInfo))]
-pub struct CustomAssetTypeId(pub u32);
-
-/// The type of security represented by a token.
-#[derive(Clone, Copy, Decode, Encode)]
-#[cfg_attr(feature = "type_info", derive(TypeInfo))]
-pub enum AssetType {
-    /// Common stock - a security that represents ownership in a corporation.
-    EquityCommon,
-    /// Preferred stock. Preferred stockholders have a higher claim to dividends or asset
-    /// distribution than common stockholders.
-    EquityPreferred,
-    /// Commodity - a basic good used in commerce that is interchangeable with other commodities of
-    /// the same type.
-    Commodity,
-    /// Fixed income security - an investment that provides a return in the form of fixed periodic
-    /// interest payments and the eventual return of principal at maturity. Examples: bonds,
-    /// treasury bills, certificates of deposit.
-    FixedIncome,
-    /// Real estate investment trust - a company that owns, operates, or finances income-producing
-    /// properties.
-    REIT,
-    /// Investment fund - a supply of capital belonging to numerous investors used to collectively
-    /// purchase securities while each investor retains ownership and control of his own shares.
-    Fund,
-    /// Revenue share partnership agreement - a document signed by all partners in a partnership
-    /// that has procedures when distributing business profits or losses.
-    RevenueShareAgreement,
-    /// Structured product, aka market-linked investment - a pre-packaged structured finance
-    /// investment strategy based on a single security, a basket of securities, options, indices,
-    /// commodities, debt issuance or foreign currencies, and to a lesser extent, derivatives.
-    StructuredProduct,
-    /// Derivative contract - a contract between two parties for buying or selling a security at a
-    /// predetermined price within a specific time period. Examples: forwards, futures, options or
-    /// swaps.
-    Derivative,
-    /// Anything else.
-    Custom(CustomAssetTypeId),
-    /// Stablecoins are cryptocurrencies designed to minimize the volatility of the price of the stablecoin,
-    /// relative to some "stable" asset or basket of assets.
-    /// A stablecoin can be pegged to a cryptocurrency, fiat money, or to exchange-traded commodities.
-    StableCoin,
-}
-
-impl Default for AssetType {
-    fn default() -> Self {
-        Self::EquityCommon
+impl From<Vec<u8>> for Encoded {
+    fn from(data: Vec<u8>) -> Self {
+        Self(data)
     }
 }
 
-/// A wrapper for a funding round name.
-#[derive(Clone, Decode, Encode)]
-#[cfg_attr(feature = "type_info", derive(TypeInfo))]
-pub struct FundingRoundName(pub Vec<u8>);
+impl<T: Encode> From<&T> for Encoded {
+    fn from(v: &T) -> Self {
+        Self(v.encode())
+    }
+}
 
-/// Implementation of common asset identifiers.
-/// https://www.cusip.com/identifiers.html.
-#[derive(Clone, Copy, Decode, Encode)]
-#[cfg_attr(feature = "type_info", derive(TypeInfo))]
-pub enum AssetIdentifier {
-    /// Universally recognized identifier for financial instruments.
-    /// Example: Amazon.com Inc - Common Stock
-    /// ISSUER ISSUE CHECK CUSIP
-    /// 023135 10    6     023135106
-    CUSIP([u8; 9]),
-    /// The CUSIP International Numbering System.
-    /// Example: Abingdon Capital PLC - Shares
-    /// COUNTRY CODE ISSUER ISSUE CHECK CINS
-    /// G            0052B  10    5     G0052B105
-    CINS([u8; 9]),
-    /// The International Securities Identification Number.
-    /// Example:
-    /// COUNTRY CODE LOCAL IDENTIFIER CHECK ISIN
-    /// CA           008911703        4     CA0089117034
-    ISIN([u8; 12]),
-    /// The Legal Entity Identifier.
-    /// Example: Philadelphia Cheesesteak Company
-    /// LOU PREFIX ENTITY INDENTIFIER VERIFICATION ID LEI
-    /// 5493       00SAMIRN1R27UP     42              549300SAMIRN1R27UP42
-    LEI([u8; 20]),
-    /// Financial Instrument Global Identifier https://www.omg.org/figi/index.htm.
-    /// Example: Alphabet Inc - Common Stock
-    /// BBG013V1S0T3
-    FIGI([u8; 12]),
+impl Encode for Encoded {
+    fn size_hint(&self) -> usize {
+        self.0.len()
+    }
+
+    fn encode_to<O: Output + ?Sized>(&self, out: &mut O) {
+        out.write(&self.0)
+    }
 }
 
 #[ink::chain_extension]
+#[derive(Clone, Copy)]
 pub trait PolymeshRuntime {
     type ErrorCode = PolymeshRuntimeErr;
 
-    #[ink(extension = 0x00_1A_00_00, returns_result = false)]
-    fn register_ticker(ticker: Ticker);
+    #[ink(extension = 0x00_00_00_01, returns_result = false)]
+    fn call_runtime(call: Encoded);
 
-    #[ink(extension = 0x00_1A_01_00, returns_result = false)]
-    fn accept_ticker_transfer(auth_id: u64);
+    #[ink(extension = 0x00_00_00_02, returns_result = false)]
+    fn read_storage(key: Encoded) -> Option<Vec<u8>>;
 
-    #[ink(extension = 0x00_1A_02_00, returns_result = false)]
-    fn accept_asset_ownership_transfer(auth_id: u64);
+    #[ink(extension = 0x00_00_00_03, returns_result = false)]
+    fn get_spec_version() -> u32;
 
-    #[ink(extension = 0x00_1A_03_00, returns_result = false)]
-    fn create_asset(
-      name: AssetName,
-      ticker: Ticker,
-      divisible: bool,
-      asset_type: AssetType,
-      identifiers: Vec<AssetIdentifier>,
-      funding_round: Option<FundingRoundName>,
-      disable_iu: bool,
-    );
+    #[ink(extension = 0x00_00_00_04, returns_result = false)]
+    fn get_transaction_version() -> u32;
+}
 
-    #[ink(extension = 0x00_1A_11_00, returns_result = false)]
-    fn register_custom_asset_type(ty: Vec<u8>);
+pub type PolymeshRuntimeInstance = <PolymeshRuntime as ink::ChainExtensionInstance>::Instance;
+
+pub fn new_instance() -> PolymeshRuntimeInstance {
+    <PolymeshRuntime as ink::ChainExtensionInstance>::instantiate()
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, scale::Encode, scale::Decode)]
-#[cfg_attr(feature = "type_info", derive(TypeInfo))]
+#[cfg_attr(feature = "std", derive(TypeInfo))]
 pub enum PolymeshRuntimeErr {
     Unknown,
 }
@@ -156,8 +81,7 @@ impl ink_env::chain_extension::FromStatusCode for PolymeshRuntimeErr {
 pub enum PolymeshEnvironment {}
 
 impl Environment for PolymeshEnvironment {
-    const MAX_EVENT_TOPICS: usize =
-        <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
+    const MAX_EVENT_TOPICS: usize = <ink_env::DefaultEnvironment as Environment>::MAX_EVENT_TOPICS;
 
     type AccountId = <ink_env::DefaultEnvironment as Environment>::AccountId;
     type Balance = <ink_env::DefaultEnvironment as Environment>::Balance;
